861329b15637519760277985f2d99eddb538a4b9
Yurii Stubrov <yurii.stubrov@gmail.com>
2017-08-15 17:00:27 +0300

    unnecessary code deleted.

--- CarTrajectory/src/CarTrajectory.cpp
+++ CarTrajectory/src/CarTrajectory.cpp
@@ -11,14 +11,15 @@
 #include <fstream>
 #include <string>
 
-#define MIN_NUM_FEAT 2000
+#define MIN_NUM_FEAT 2000 //features threshold
 
-void TrackFeatures(cv::Mat aImage1, cv::Mat aImage2, std::vector<cv::Point2f>& aPoints1, std::vector<cv::Point2f>& aPoints2, std::vector<uchar>& aStatus) {
-
+//Tracks detected features from one image in another image
+void TrackFeatures(cv::Mat aImage1, cv::Mat aImage2, std::vector<cv::Point2f>& aPoints1, std::vector<cv::Point2f>& aPoints2, std::vector<uchar>& aStatus) 
+{
 	// This function automatically gets rid of points for which tracking fails
 	std::vector<float> err;
 
-	// Optical Flow Algorithm Lucas-Kanade
+	// Optical Flow estimation using Lucas-Kanade method
 	cv::calcOpticalFlowPyrLK(aImage1, aImage2, aPoints1, aPoints2, aStatus, err);
 
 	// Getting rid of points for which the KLT tracking failed or those who have gone outside the frame
@@ -39,12 +40,15 @@ void TrackFeatures(cv::Mat aImage1, cv::Mat aImage2, std::vector<cv::Point2f>& a
 	}
 }
 
-
-void DetectFeatures(cv::Mat aImage, std::vector<cv::Point2f>& aPoints) //uses FAST as of now, modify parameters as necessary
+// Uses FAST algorithm for feature detection. Converts the datatype of the 
+// detected feature points from KeyPoints to a vector of Point2f, so that we
+// can directly pass it to the feature tracking step.
+void DetectFeatures(cv::Mat aImage, std::vector<cv::Point2f>& aPoints) 
 {
 	std::vector<cv::KeyPoint> keypoints;
-	int fastThreshold = 20;
+	const int fastThreshold = 20;
 	bool nonmaxSuppression = true;
+
 	// FAST features detection
 	cv::FAST(aImage, keypoints, fastThreshold, nonmaxSuppression);
 	cv::KeyPoint::convert(keypoints, aPoints, std::vector<int>());
@@ -52,7 +56,7 @@ void DetectFeatures(cv::Mat aImage, std::vector<cv::Point2f>& aPoints) //uses FA
 
 int main(int argc, char** argv)
 {
-	int frameNumber;
+	int frameNumber; // Amount of images we have to construct a trajectory of a moving car
 	std::string dataDir;
 	std::string calibFilePath;
 	
@@ -98,12 +102,14 @@ int main(int argc, char** argv)
 	std::vector<uchar> status;
 	TrackFeatures(image1, image2, points1, points2, status); // Track those features to image2
 
-	std::ifstream file2;
+	std::ifstream file2; // contains our calibration data
 
 	file2.open(calibFilePath.c_str());
 	std::string str;
 	std::vector<double> calib;
 
+	// get the calibration data from file2 line by line and 
+	// put it in vector calib.
 	while (std::getline(file2, str))
 	{
 		std::istringstream stream(str.substr(4));
@@ -117,7 +123,9 @@ int main(int argc, char** argv)
 	double focal = calib[0];
 	const cv::Point2d pp(calib[2], calib[6]);
 
-	// Recovering the pose and the essential matrix
+	// Calculating an essential matrix from the corresponding points in
+	// two images using Nister's 5-point algorithm with RANSAC and 
+	// recovering relative camera rotation and translation 
 	cv::Mat essentialMatrix;
 	cv::Mat R;
 	cv::Mat t;
@@ -142,6 +150,7 @@ int main(int argc, char** argv)
 		   << "Translation vector: " << std::endl << "[" << translationVector.at<double>(0) << " "
 		   << translationVector.at<double>(1) << " " << translationVector.at<double>(2) << "]" << std::endl;
 
+	// Performing all these steps for the following images
 	for (int frameCount = 2; frameCount < frameNumber; ++frameCount)
 	{
 		std::string path = dataDir + "%06d.png";
@@ -153,31 +162,16 @@ int main(int argc, char** argv)
 
 		essentialMatrix = cv::findEssentialMat(currFeatures, prevFeatures, focal, pp, cv::RANSAC, 0.999, 1.0, mask);
 		cv::recoverPose(essentialMatrix, currFeatures, prevFeatures, R, t, focal, pp, mask);
-
-		cv::Mat prevPts(2, (int)prevFeatures.size(), CV_64F);
-		cv::Mat currPts(2, (int)currFeatures.size(), CV_64F);
-		
-		int prevFeaturesSize = (int)prevFeatures.size();
-		for (int i = 0; i < prevFeaturesSize; ++i)
-		{
-			prevPts.at<double>(0, i) = prevFeatures.at(i).x;
-			prevPts.at<double>(1, i) = prevFeatures.at(i).y;
-
-			currPts.at<double>(0, i) = currFeatures.at(i).x;
-			currPts.at<double>(1, i) = currFeatures.at(i).y;
-		}
-
-		if ((t.at<double>(2) > t.at<double>(0)) && (t.at<double>(2) > t.at<double>(1)))
-		{
-			translationVector = translationVector + scale * (rotationMatrix * t);
-			rotationMatrix = R * rotationMatrix;
-
-		}
+	
+        translationVector = translationVector + scale * (rotationMatrix * t);
+	    rotationMatrix = R * rotationMatrix;
+	
 
 		file1 << "image" << frameCount << std::endl << "Rotation Matrix: " << std::endl << rotationMatrix << std::endl
-			   << "Translation vector: " << std::endl << "[" << translationVector.at<double>(0) << " "
-			   << translationVector.at<double>(1) << " " << translationVector.at<double>(2) << "]" << std::endl;
+			  << "Translation vector: " << std::endl << "[" << translationVector.at<double>(0) << " "
+			  << translationVector.at<double>(1) << " " << translationVector.at<double>(2) << "]" << std::endl;
 
+		// if the number of features drop below a certain threshold a new feature detection is needed
 		if (prevFeatures.size() < MIN_NUM_FEAT)
 		{
 			DetectFeatures(prevImage, prevFeatures);
